f5ff03e0fd1ef979fe1ce05d656e856dafbc48a1
starshibrat <saddamkusuma9029@gmail.com>
2024-12-02 14:12:23 +0700

    change matrice video

--- src/pages/lectures/matrix/MatrixVideoPage.jsx
+++ src/pages/lectures/matrix/MatrixVideoPage.jsx
@@ -7,7 +7,7 @@ export default function MatrixVideoPage() {
         <Course title="Matriks" path={paths}>
 
             <div className="embed-responsive embed-responsive-16by9">
-                <iframe className="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/lTfGBCe-vpk?si=ObAp2aYbripPOAEV" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                <iframe className="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/yRwQ7A6jVLk?si=r5HNWrPm7W36uLjH" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             </div>
         </Course>
     )
