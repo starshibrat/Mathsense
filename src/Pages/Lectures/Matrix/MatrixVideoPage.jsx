c4af71208131c1ac4144fa794be36aeedc6c9e1d
starshibrat <saddamkusuma9029@gmail.com>
2024-12-23 07:11:33 +0700

    modify course page aethestic and more content to matriks video

--- src/Pages/Lectures/Matrix/MatrixVideoPage.jsx
+++ src/Pages/Lectures/Matrix/MatrixVideoPage.jsx
@@ -7,9 +7,23 @@ export default function MatrixVideoPage() {
     const paths = [path + "/materi", path + "/video", path + "/quiz"];
     return (
         <Course title="Matriks" path={paths}>
-            <div className="embed-responsive embed-responsive-16by9">
-                <iframe className="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/yRwQ7A6jVLk?si=r5HNWrPm7W36uLjH" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <div>
+                <p>Pengenalan matriks, ordo matriks, dan operasi matriks seperti penjumlahan, pengurangan, dan skalar (Bahasa Inggris)</p>
+                <div className="embed-responsive embed-responsive-16by9 text-center">
+                    <iframe className="embed-responsive-item" width="840" height="480" src="https://www.youtube.com/embed/yRwQ7A6jVLk?si=r5HNWrPm7W36uLjH" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                </div>
+
             </div>
+            <div>
+                <p>Pengenalan matriks dan jenis-jenisnya seperti matriks bujursangkar, matriks identitas. Penjelasan tentang ordo matriks, operasi-operasi pada matriks seperti penjumlahan, penguragan dan perkalian. Di video ini juga terdapat contoh soal (Bahasa Indonesia)</p>
+                <div className="embed-responsive embed-responsive-16by9 text-center">
+                    <iframe className="embed-responsive-item" width="840" height="480" src="https://www.youtube.com/embed/-2pSwVmHySY?si=zH1GsW6r-cigkRll" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                </div>
+
+            </div>
+
+            
+
         </Course>
     )
 }
--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -2,16 +2,24 @@ import { ListGroup, Stack } from "react-bootstrap"
 import setTitle from "./PageFunctions"
 import { Link } from "react-router-dom"
 import Body from "../Components/Body"
+import Greet from "../Components/Greet"
+import matThumbnail from "../Assets/matrix/matrices.png";
 
 export default function CoursesPage() {
     setTitle("Beginner Courses")
     return (
         <Body sidebar>
-            <Stack direction="vertical">
-                <ListGroup variant="flush" className="beginnerlist">
+            <Stack direction="horizontal">
+                {/* <ListGroup variant="flush" className="beginnerlist">
                     <Link to="/courses/matriks/materi"><ListGroup.Item>Matriks</ListGroup.Item></Link>
                     <ListGroup.Item>Limit</ListGroup.Item>
-                </ListGroup>
+                </ListGroup> */}
+                <Greet
+                    title={"Matriks"}
+                    description={"Sekumpulan bilangan yang disusun berdasarkan baris dan kolom, dan diapit dengan tanda kurung"}
+                    imgLink={matThumbnail}
+                    path={"/courses/matriks/materi"}
+                />
             </Stack>
         </Body>
     )
